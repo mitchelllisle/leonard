4d45b934fc711236dd275883a49b11c56b225ab7
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2018-03-31 16:48:26 +1100

    added docs

--- leonard/summaries.py
+++ leonard/summaries.py
@@ -2,11 +2,19 @@ def distribution(data, column):
     '''
     Distribution of data based off Categories in column
     
-    @description This functions works off categorical data. It 
-    will return the frequency and percentage of data points that fit to each distinct category.
+    This functions works off categorical data. It will return the frequency and percentage 
+    of data points that fit to each distinct category.
     
-    @param data that contains the column you're interested in checking
-    @param column the column to run distribution checks
+    ------
+    PARAMS
+    ------
+    data : that contains the column you're interested in checking
+    column : the column to run distribution checks
+    
+    --------
+    EXAMPLES
+    --------
+    Coming soon......
     '''
     dist = (
         data
