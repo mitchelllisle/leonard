a35629e40ff18e7c9b879af87b62303750199cd8
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2018-05-14 19:19:09 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 from setuptools.command.install import install
 
 setup(name='martha',
-      version='0.1',
+      version='SpeedySole',
       description='A package for Exploratory Data Analysis',
       url='http://github.com/mitchelllisle/martha',
       author='Mitchell Lisle',
