5ab80efe499fd03d463f49c57be74642eb501328
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2018-06-30 14:26:25 +1000

    add more tests

--- tests/test_summaries.py
+++ tests/test_summaries.py
@@ -48,7 +48,14 @@ def test_showNullColumns():
 
     assert len(result) == 1
 
-def test_checkMissingDates():
+def test_checkMissingDates_1():
     dates = pd.Series(['2017-01-01', '2017-01-02', '2017-01-04', '2017-01-06'], name = "dates")
     missingDates = checkMissingDates(dates, returnType = 'missing')
+    allDates = checkMissingDates(dates, returnType = 'all')
     assert len(missingDates == 2)
+    assert len(allDates == 6)
+
+def test_checkMissingDates_2():
+    dates = pd.Series(['2017-01-01', '2017-01-02', '2017-01-04', '2017-01-06'], name = "dates")
+    missingDates = checkMissingDates(dates, returnType = 'viz')
+    assert type(missingDates) == alt.Chart
