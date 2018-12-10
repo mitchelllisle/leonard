ccffd7236cb43b8689002e7bf801b33c53ee5161
Mitchell Lisle <lislemitchell@gmail.com>
2018-12-10 19:41:36 +1100

    added eda

--- martha/eda.py
+++ martha/eda.py
@@ -1,11 +1,15 @@
 import numpy as np
 from scipy.stats import gaussian_kde
-import c3p0 as c3
-import martha as mh
+from preProcessing import normalise
 import altair as alt
 
 
 class DensityPlot:
+    """
+    DensityPlot: This class is used to construct a density plot. A number of the ideas and
+    implementation was inspired by this post: http://www.nathan-rice.net/eda-python/
+    """
+
     def __init__(self, data, X):
         self.data = data
         self.x = X
@@ -20,7 +24,7 @@ class DensityPlot:
         self.data[self.x] = self.data[self.x].map(int)
         self.__data__ = self.data.groupby(self.x).agg({self.x: 'count'}).rename({self.x: 'count'}, axis=1).reset_index()
         self.__data__.sort_values(self.x)
-        self.__data__['histogram_y'] = mh.normalise(self.__data__['count'])
+        self.__data__['histogram_y'] = normalise(self.__data__['count'])
 
         chart = alt.Chart(self.__data__).mark_bar(
             color="cornflowerblue"
@@ -36,12 +40,12 @@ class DensityPlot:
         de = gaussian_kde(X)
 
         self.__data__['gk_interpolated'] = de(np.array(self.__data__[self.x].map(int)))
-        self.__data__['gk_interpolated_norm'] = mh.normalise(self.__data__['gk_interpolated'])
+        self.__data__['gk_interpolated_norm'] = normalise(self.__data__['gk_interpolated'])
 
         chart = alt.Chart(self.__data__).mark_area(
             fill=plot_color,
-            opacity=0.5,
-            line={"color": plot_color, "strokeDash": [5, 2]}
+            fillOpacity=0.6,
+            line={"color": plot_color, "strokeDash": [3, 2]}
         ).encode(
             x=alt.X(self.x, type="quantitative", title=self.x),
             y=alt.Y('gk_interpolated_norm', stack=None, title=self.y_label)
@@ -52,12 +56,12 @@ class DensityPlot:
         plot_color = "#7E52A8"
 
         self.__data__['cgk_interpolated'] = np.cumsum(self.__data__['gk_interpolated'])
-        self.__data__['cgk_interpolated_norm'] = mh.normalise(self.__data__['cgk_interpolated'])
+        self.__data__['cgk_interpolated_norm'] = normalise(self.__data__['cgk_interpolated'])
 
         chart = alt.Chart(self.__data__).mark_area(
             fill=plot_color,
-            opacity=0.5,
-            line={"color": plot_color, "strokeDash": [5, 2]}
+            fillOpacity=0.6,
+            line={"color": plot_color, "strokeDash": [3, 2]}
         ).encode(
             x=alt.X(self.x, type="quantitative", title=self.x),
             y=alt.Y('cgk_interpolated_norm', stack=None, title=self.y_label)
@@ -80,22 +84,5 @@ def densityPlot(data, x, width=800, height=400):
         width=width,
         height=height,
         title='Coefficient of variation'
-    ).configure_axis(
-
     )
     return chart
-
-
-env = c3.source()
-db = env['mlprojects']
-
-data = c3.queryPostgres(
-    db['host'],
-    db['port'],
-    db['username'],
-    db['password'],
-    "crashes",
-    "select * from events limit 1000"
-)
-
-densityPlot(data, 'speed_limit')
